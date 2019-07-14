84c77e8927fc643e0f5de6de7bb61d7e9ec38bd4
juju4 <juju4@users.noreply.github.com>
2019-07-14 16:15:47 -0400

    update role dependencies

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -5,7 +5,7 @@ umask 022
 
 rolesdir=$(dirname $0)/..
 
-[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+[ ! -d $rolesdir/juju4.redhat_epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat_epel
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.sysdigcapture ] && ln -s ansible-sysdigcapture $rolesdir/juju4.sysdigcapture
 [ ! -e $rolesdir/juju4.sysdigcapture ] && cp -R $rolesdir/ansible-sysdigcapture $rolesdir/juju4.sysdigcapture
